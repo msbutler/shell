bdf2e049e2e826bf8caaf2834b278d5b5b0ef87e
Michael Butler <butler@cockroachlabs.com>
2022-11-23 11:42:40 -0500

    add cla function

--- setups/cockroach.sh
+++ setups/cockroach.sh
@@ -6,6 +6,10 @@ if [ -f ~/.cockroach.lic ]; then
   export COCKROACH_DEV_LICENSE="$(cat ~/.cockroach.lic)"
 fi
 
+if [ -f ~/.github_token ]; then                                                
+   export GITHUB_TOKEN="$(cat ~/.github_token)"                        
+fi  
+
 # roachprod
 export CLUSTER=butler-test
 ROACHPROD_USER=butler
@@ -17,3 +21,8 @@ export ALWAYS_RUN_GAZELLE=1
 export MACOSX_DEPLOYMENT_TARGET=12.0
 
 
+function cla() {
+  curl -H "Authorization: token $GITHUB_TOKEN" \
+    -d '{"state": "success", "context":"license/cla", "description": "curl"}' \
+    https://api.github.com/repos/cockroachdb/cockroach/statuses/$1
+}
