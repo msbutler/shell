fa39cb5a9b7818601fb66ca6b3456eabafca5881
Michael Butler <butler@cockroachlabs.com>
2022-12-02 18:12:48 -0500

    cockroach binaries helper

--- setups/cockroach.sh
+++ setups/cockroach.sh
@@ -26,3 +26,29 @@ function cla() {
     -d '{"state": "success", "context":"license/cla", "description": "curl"}' \
     https://api.github.com/repos/cockroachdb/cockroach/statuses/$1
 }
+
+# Specify  master or a release (e.g. v22.1.5) and pull binary to /bin
+# If master is specified, the workload binary also gets pulled.
+function binaries() {
+ roachprod destroy local 
+ roachprod create -n 1 local
+ 
+ mkdir bin
+
+ if [[ "$1" == "master" ]]; then
+  roachprod stage local cockroach --os linux
+  
+  # Automatically get and copy workload to /bin directory
+  roachprod stage local workload --os linux
+  mv ~/local/1/workload bin/workload
+ else
+  roachprod stage local release $1 --os linux
+ fi
+ 
+
+ # Copy it from (local) node 1 to the current directory
+ mv ~/local/1/cockroach bin/cockroach
+ 
+ roachprod destroy local
+ echo "Binaries in /bin" 
+}
