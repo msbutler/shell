8d25b4ad866efcbb0ee4fb363f2241ae19b43f69
Michael Butler <butler@cockroachlabs.com>
2022-12-19 19:16:42 -0500

    fix python path

--- py.sh
+++ py.sh
@@ -1,8 +1,10 @@
 
 # install python3 via `sudo apt-get install python-is-python3`
 # to ensure that python3 is in the default path
-if [[ -d /usr/bin/python ]]; then
+if [[ -e /usr/bin/python ]]; then
   export PATH="/usr/bin/python:$PATH"
-elif [[ -d /usr/local/bin/python ]]; then
-  export PATH="/usr/local/bin/python:$PATH"
+elif [[ -e /usr/local/bin/python3 ]]; then
+  # brew version
+  export PATH="/usr/local/bin/python3:$PATH"
+  alias python=/usr/local/bin/python3
 fi
